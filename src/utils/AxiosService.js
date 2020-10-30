9f5863c5e2e87d81388bd4e1c655e6f74983656a
MC Labs <molconlabs@gmail.com>
2020-10-30 16:02:13 +0530

    Cleaning Branch

--- src/utils/AxiosService.js
+++
@@ -1,9 +0,0 @@
-import Axios from "axios";
-
-/* Service layer class for all API related calls */
-
-export const getAllMappings = async config => {
-  return await Axios.request(config).then(function(response) {
-    return Promise.resolve(response.data);
-  });
-};
